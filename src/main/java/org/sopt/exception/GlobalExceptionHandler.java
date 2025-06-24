3700bdc45818bedd1d0c6b0f07b0784877360047
jeong724 <oliveyou724@gmail.com>
2025-06-24 18:08:02 +0900

    feat : 에러 핸들러 예외 추가 @NotNull로 변경

--- src/main/java/org/sopt/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/exception/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package org.sopt.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.sopt.common.CommonApiResponse;
+import org.sopt.common.CommonErrorCode;
 import org.sopt.common.ErrorCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +18,29 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CommonApiResponse<?>> handleCustomException(CustomException ex) {
         ErrorCode errorCode = ex.getErrorCode();
 
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(CommonApiResponse.onFailure(errorCode));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CommonApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        ErrorCode errorCode = CommonErrorCode.INVALID_INPUT_VALUE;
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(CommonApiResponse.onFailure(errorCode));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<CommonApiResponse<Void>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e){
+        ErrorCode errorCode = CommonErrorCode.MISSING_PARAMETER;
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(CommonApiResponse.onFailure(errorCode));
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonApiResponse<Void>> handleUnexpectedException(Exception e) {
+        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(CommonApiResponse.onFailure(errorCode));
